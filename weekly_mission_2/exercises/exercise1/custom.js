411d84edf9ac89fbdee9a1f7ad27462fc8d0e8f9
Alejandro <alejandrom.am33@gmail.com>
2022-04-16 21:17:53 -0500

    add: my objects exercise 1

---
+++ weekly_mission_2/exercises/exercise1/custom.js
@@ -0,0 +1,16 @@
+const uber = {
+    username: 'Alejandro',
+    uberPass: true,
+    paymentMethods: ['Credit card', 'Paypal', 'Debit card', 'Cash'],
+    accountTime: '1 year',
+}
+
+const fb = {
+    username: 'Alejandro Morales',
+    numberOfFriends: '1.5k',
+    accountTime: '5 years',
+    posts: 259,
+    likes: '1.5k',
+    comments: '1k',
+    shares: '1k',
+}
